48a0ed4ad75d1f973c5cb4d84d1dc499e477ecd2
Kayode Oluwasegun <segunkayode00@gmail.com>
2023-11-14 10:52:29 +0100

    wip: hw6

--- programming_language_c_ruby/hw6/hw6assignment.rb
+++ programming_language_c_ruby/hw6/hw6assignment.rb
@@ -3,6 +3,11 @@
 # This is the only file you turn in, so do not modify the other files as
 # part of your solution.
 
+# we can use the these in my solution:
+# - Your code can also use the classes and methods in
+# hw6graphics.rb (as well as the classes and methods in hw6provided.rb), except for the methods
+# marked with comments as not to be called by student code
+
 class MyPiece < Piece
     # The constant All_My_Pieces should be declared here
   
@@ -17,7 +22,11 @@ class MyPiece < Piece
   
   class MyTetris < Tetris
     # your enhancements here
-  
+    def key_bindings
+      k_b = super
+      k_b
+      @root.bind("u", proc {@board.rotate_clockwise; @board.rotate_clockwise})
+    end
   end
   
   
